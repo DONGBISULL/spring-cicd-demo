f5ae130a0401e34cb8d96bce7d77dd4d7cfa0811
SDB <sdb7734@gmail.com>
2025-09-14 16:00:27 +0900

    init commit

---
+++ src/test/java/dev/exam/springcicd/SpringCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.exam.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
